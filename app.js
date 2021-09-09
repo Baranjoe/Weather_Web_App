73debd789544105a3c6e839665eab89593739aec
Barandun <j.barandun@lincolninternational.ch>
2021-09-09 19:50:30 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.send("The server is up and running.")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("The server is running on port 3000.")
+});
