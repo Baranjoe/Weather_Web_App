2caa8906d8ed4a1aa959cb207d18bc9be66ed792
Barandun <j.barandun@lincolninternational.ch>
2021-09-09 20:10:34 +0200

    Using API

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=590ebb48c0078949a72866f234fbe42e"
+
+  https.get(url, function(response) {
+    console.log(response);
+  });
+
+
+
   res.send("The server is up and running.")
 });
 
